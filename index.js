3f787815c3b3f3fd286f84be43d28012033bbfff
Luísa Dornelas <87919798+Dornelass@users.noreply.github.com>
2023-03-23 14:21:04 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+// var center1 = document.getElementById("center")
+
+function abrir () {
+    document.getElementById('popup').style.display =  'block'
+}
+function fechar () {
+    document.getElementById('popup').style.display =  'none'
+}
